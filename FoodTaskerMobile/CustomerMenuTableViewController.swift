9ec294bb1749e417295bbc2d2db6d0a4cf738e6a
Ryoko Tominaga <ryokotmng@gmail.com>
2018-03-03 22:53:41 +0900

    fixed restaurant page and sidebar color

---
+++ FoodTaskerMobile/CustomerMenuTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CustomerMenuTableViewController.swift
+//  FoodTaskerMobile
+//
+//  Created by Ryoko Tominaga on 2018/03/03.
+//  Copyright © 2018年 Ryoko Tominaga. All rights reserved.
+//
+
+import UIKit
+
+class CustomerMenuTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor(red: 0.19, green: 0.18, blue: 0.31, alpha: 1.0)
+    }
+}
--- FoodTaskerMobile/RestaurantViewController.swift
+++ FoodTaskerMobile/RestaurantViewController.swift
@@ -20,6 +20,18 @@ class RestaurantViewController: UIViewController {
             (SWRevealViewController.revealToggle(_:))
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
     }
+}
 
-
+extension RestaurantViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCell", for: indexPath)
+        
+        return cell
+    }
 }
