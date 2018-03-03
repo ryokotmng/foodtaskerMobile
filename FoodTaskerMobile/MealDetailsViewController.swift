5dd009ed09ce55a511dbcba0323c5319c28e080d
Ryoko Tominaga <ryokotmng@gmail.com>
2018-03-04 01:22:15 +0900

    meal details page created

---
+++ FoodTaskerMobile/MealDetailsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MealDetailsViewController.swift
+//  FoodTaskerMobile
+//
+//  Created by Ryoko Tominaga on 2018/03/04.
+//  Copyright © 2018年 Ryoko Tominaga. All rights reserved.
+//
+
+import UIKit
+
+class MealDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
