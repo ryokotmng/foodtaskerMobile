9b81a2507a29f13635e6a9281991cd9decc8502c
Ryoko Tominaga <ryokotmng@gmail.com>
2018-03-07 08:09:51 +0900

    create class for order view controller

---
+++ FoodTaskerMobile/OrderViewController.swift
@@ -0,0 +1,41 @@
+//
+//  OrderViewController.swift
+//  FoodTaskerMobile
+//
+//  Created by Ryoko Tominaga on 2018/03/07.
+//  Copyright © 2018年 Ryoko Tominaga. All rights reserved.
+//
+
+import UIKit
+
+class OrderViewController: UIViewController {
+
+    @IBOutlet weak var menuBarButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if self.revealViewController() != nil {
+            menuBarButton.target = self.revealViewController()
+            menuBarButton.action = #selector
+                (SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+}
+
+extension OrderViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OrderItemCell", for: indexPath)
+        
+        return cell
+    }
+}
