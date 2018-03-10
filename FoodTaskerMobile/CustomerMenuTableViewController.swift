58a3eb4ea9c616a201c1bebdb2f6930789fbabe1
Ryoko Tominaga <ryokotmng@gmail.com>
2018-03-11 00:49:55 +0900

    show user image

--- FoodTaskerMobile/CustomerMenuTableViewController.swift
+++ FoodTaskerMobile/CustomerMenuTableViewController.swift
@@ -9,9 +9,19 @@
 import UIKit
 
 class CustomerMenuTableViewController: UITableViewController {
-
+    
+    @IBOutlet weak var imgAvatar: UIImageView!
+    @IBOutlet weak var lbName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        lbName.text = User.currentUser.name
+        imgAvatar.image = try! UIImage(data: Data(contentsOf: URL(string: User.currentUser.pictureURL!)!))
+        imgAvatar.layer.cornerRadius = 70 / 2
+        imgAvatar.layer.borderWidth = 1.0
+        imgAvatar.layer.borderColor = UIColor.white.cgColor
+        imgAvatar.clipsToBounds = true
         
         view.backgroundColor = UIColor(red: 0.19, green: 0.18, blue: 0.31, alpha: 1.0)
     }
