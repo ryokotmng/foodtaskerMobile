9cde9ff7fea1fc1d3df7cd18e1fa28edbcaf339a
Ryoko Tominaga <ryokotmng@gmail.com>
2018-03-17 16:05:12 +0900

    create login and logout API

--- FoodTaskerMobile/Manager/APIManager.swift
+++ FoodTaskerMobile/Manager/APIManager.swift
@@ -14,7 +14,7 @@ import FBSDKLoginKit
 class APIManager {
     static let shared = APIManager()
     
-    let baseURL = NSURL(string: "localhost:8000/")
+    let baseURL = NSURL(string: BASE_URL)
     
     var accessToken = ""
     var refreshToken = ""
@@ -23,8 +23,57 @@ class APIManager {
     // APIs to login and out
     func login(userType: String, completionHandler: @escaping (NSError?) -> Void) {
         
+        let path = "api/social/convert-token"
+        let url = baseURL!.appendingPathComponent(path)
+        let params: [String : Any] = [
+            "grant_type": "convert-token",
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+            "backend": "facebook",
+            "token": FBSDKAccessToken.current().tokenString,
+            "user_type": userType
+        ]
+        
+        Alamofire.request(url!, method: .post, parameters: params, encoding: URLEncoding(), headers: nil).responseJSON{ (response) in
+            
+            switch response.result {
+            case .success(let value):
+                
+                let jsonData = JSON(value)
+                
+                self.accessToken = jsonData["access_token"].string!
+                self.refreshToken = jsonData["refresh_token"].string!
+                self.expired = Date().addingTimeInterval(TimeInterval(jsonData["expires_in"].int!))
+                
+                completionHandler(nil)
+                break
+                
+            case .failure(let error):
+                completionHandler(error as NSError?)
+                break
+            }
+        }
     }
     func logout(completionHandler: @escaping (NSError?) -> Void) {
         
+        let path = "api/social/revoke-token"
+        let url = baseURL!.appendingPathComponent(path)
+        let params: [String: Any] = [
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+            "token": self.accessToken
+        ]
+        
+        Alamofire.request(url!, method: .post, parameters: params, encoding: URLEncoding(), headers: nil).responseString { (response) in
+            switch response.result {
+            case .success:
+                completionHandler(nil)
+                break
+
+            case .failure(let error):
+                completionHandler(error as NSError?)
+                break
+            }
+        }
     }
 }
---
+++ FoodTaskerMobile/Manager/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  FoodTaskerMobile
+//
+//  Created by Ryoko Tominaga on 2018/03/11.
+//  Copyright © 2018年 Ryoko Tominaga. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL: String = "http://localhost:8000"
+let CLIENT_ID: String = ""
+let CLIENT_SECRET: String = ""
+
+let USERTYPE_CUSTOMER = "customer"
+let USERTYPE_DRIVER = "driver"
+
