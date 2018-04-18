a179fe9fdcd2d5ac2c55c0788544508c71b5e3ea
Ryoko Tominaga <ryokotmng@gmail.com>
2018-04-18 23:30:49 +0900

    add animation button to button

--- FoodTaskerMobile/Controller/MealDetailsViewController.swift
+++ FoodTaskerMobile/Controller/MealDetailsViewController.swift
@@ -40,6 +40,16 @@ class MealDetailsViewController: UIViewController {
     }
     
     @IBAction func addToTray(_ sender: Any) {
+        let image = UIImageView(frame: CGRect(x: 0, y: 0, width: 60, height: 40))
+        image.image = UIImage(named: "button_chicken")
+        image.center = CGPoint(x: self.view.frame.width/2, y: self.view.frame.height-100)
+        self.view.addSubview(image)
+        
+        UIView.animate(withDuration: 1.0,
+                       delay: 0.0,
+                       options: UIViewAnimationOptions.curveEaseOut,
+                       animations: { image.center = CGPoint(x: self.view.frame.width - 40, y: 24) },
+                       completion: { _ in image.removeFromSuperview() })
     }
     @IBAction func removeQty(_ sender: Any) {
         if qty >= 2 {
