e2c5e4cc4afa26412b62a9939500065ab1924471
Ryoko Tominaga <ryokotmng@gmail.com>
2018-04-22 22:55:23 +0900

    general logic to display tray view

--- FoodTaskerMobile/Controller/TrayViewController.swift
+++ FoodTaskerMobile/Controller/TrayViewController.swift
@@ -7,10 +7,21 @@
 //
 
 import UIKit
+import MapKit
 
 class TrayViewController: UIViewController {
 
     @IBOutlet weak var menuBarButton: UIBarButtonItem!
+    
+    @IBOutlet weak var tbvMeals: UITableView!
+    @IBOutlet weak var viewTotal: UIView!
+    @IBOutlet weak var viewAddress: UIView!
+    @IBOutlet weak var viewMap: UIView!
+    @IBOutlet weak var lbTotal: UILabel!
+    @IBOutlet weak var tbAddress: UITextField!
+    @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var bAddPayment: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +32,33 @@ class TrayViewController: UIViewController {
                 (SWRevealViewController.revealToggle(_:))
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
+        
+        if Tray.currentTray.items.count == 0 {
+            // Showing a message here
+            
+            let lbEmptyTray = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 40))
+            lbEmptyTray.center = self.view.center
+            lbEmptyTray.textAlignment = NSTextAlignment.center
+            lbEmptyTray.text = "Your tray is empty. Please select meal."
+            
+            self.view.addSubview(lbEmptyTray)
+            
+        } else {
+            // Display all of the UI controllers
+            
+            self.tbvMeals.isHidden = false
+            self.viewTotal.isHidden = false
+            self.viewAddress.isHidden = false
+            self.viewMap.isHidden = false
+            self.bAddPayment.isHidden = false
+            
+            loadMeals()
+        }
+    }
+    
+    func loadMeals() {
+        self.tbvMeals.reloadData()
+        self.lbTotal.text = "$\(Tray.currentTray.getTotal())"
     }
 }
 
