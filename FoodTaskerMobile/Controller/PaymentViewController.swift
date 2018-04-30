067ba9f35b3f1fb1c149c76bd6ab1717db4c8a83
Ryoko Tominaga <ryokotmng@gmail.com>
2018-04-30 10:42:56 +0900

    add logic to payment process

--- FoodTaskerMobile/Controller/PaymentViewController.swift
+++ FoodTaskerMobile/Controller/PaymentViewController.swift
@@ -15,4 +15,42 @@ class PaymentViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    @IBAction func placeOrder(_ sender: Any) {
+        APIManager.shared.getLatestOrder { (json) in
+            
+            if json["order"]["status"] == nil || json["order"]["status"] == "Delivered" {
+                // Processing the payment and create an Order
+                
+                let card = self.cardTextField.cardParams
+                
+                STPAPIClient.shared().createToken(withCard: card, completion: { (token, error) in
+                    
+                    if let myError = error {
+                        print("Error:", myError)
+                    } else if let stripeToken = token {
+                        
+                        APIManager.shared.createOrder(stripeToken: stripeToken.tokenId) { (json) in
+                            Tray.currentTray.reset()
+                            self.performSegue(withIdentifier: "ViewOrder", sender: self)
+                        }
+                    }
+                })
+                
+            } else {
+                // Showing an alert message.
+                
+                let cancelAction = UIAlertAction(title: "OK", style: .cancel)
+                let okAction = UIAlertAction(title: "Go to order", style: .default, handler: { (action) in
+                    self.performSegue(withIdentifier: "ViewOrder", sender: self)
+                })
+                
+                let alertView = UIAlertController(title: "Already Order?", message: "Your current order isn't completed", preferredStyle: .alert)
+                
+                alertView.addAction(okAction)
+                alertView.addAction(cancelAction)
+                
+                self.present(alertView, animated: true, completion: nil)
+            }
+        }
+    }
 }
