f379a0cc9ed1934edfa1ed0d1d97219e78215d6f
Ryoko Tominaga <ryokotmng@gmail.com>
2018-05-01 10:36:07 +0900

    add side menu and orders list for drivers

---
+++ FoodTaskerMobile/Controller/DriverOrdersViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DriverOrdersViewController.swift
+//  FoodTaskerMobile
+//
+//  Created by Ryoko Tominaga on 2018/05/01.
+//  Copyright © 2018年 Ryoko Tominaga. All rights reserved.
+//
+
+import UIKit
+
+class DriverOrdersViewController: UITableViewController {
+
+    @IBOutlet weak var menuBarButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if self.revealViewController() != nil {
+            menuBarButton.target = self.revealViewController()
+            menuBarButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
