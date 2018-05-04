f52af183c1d2736d617dbac24140d5f88a37c65e
Ryoko Tominaga <ryokotmng@gmail.com>
2018-05-04 23:18:06 +0900

    Design revenue statistic layout

---
+++ FoodTaskerMobile/Controller/Driver/StatisticViewController.swift
@@ -0,0 +1,24 @@
+//
+//  StatisticViewController.swift
+//  FoodTaskerMobile
+//
+//  Created by Ryoko Tominaga on 2018/05/04.
+//  Copyright © 2018年 Ryoko Tominaga. All rights reserved.
+//
+
+import UIKit
+
+class StatisticViewController: UIViewController {
+
+    @IBOutlet weak var menuBarButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if self.revealViewController() != nil {
+            menuBarButton.target = self.revealViewController()
+            menuBarButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+
+    }
+}
