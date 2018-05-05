4da6c666674508095d5b1841452a6448b9439777
Ryoko Tominaga <ryokotmng@gmail.com>
2018-05-05 10:12:53 +0900

    display data on ready orders view

--- FoodTaskerMobile/Controller/Driver/DriverOrdersViewController.swift
+++ FoodTaskerMobile/Controller/Driver/DriverOrdersViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class DriverOrdersViewController: UITableViewController {
+    
+    var orders = [DriverOrder]()
+    let activityIndicator = UIActivityIndicatorView()
 
     @IBOutlet weak var menuBarButton: UIBarButtonItem!
     override func viewDidLoad() {
@@ -18,6 +21,31 @@ class DriverOrdersViewController: UITableViewController {
             menuBarButton.target = self.revealViewController()
             menuBarButton.action = #selector(SWRevealViewController.revealToggle(_:))
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        loadReadyOrders()
+    }
+    
+    func loadReadyOrders() {
+        Helpers.showActivityIndicator(activityIndicator, self.view)
+        
+        APIManager.shared.getDriverOrders { (json) in
+            
+            if json != nil {
+                
+                self.orders = []
+                if let readyOrders = json["orders"].array {
+                    for item in readyOrders {
+                        let order = DriverOrder(json: item)
+                        self.orders.append(order)
+                    }
+                }
+                
+                self.tableView.reloadData()
+                Helpers.hideActivityIndicator(self.activityIndicator)
+            }
         }
     }
 
@@ -30,11 +58,20 @@ class DriverOrdersViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 3
+        return self.orders.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "DriverOrdersCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DriverOrdersCell", for: indexPath) as! DriverOrderCell
+        
+        let order = orders[indexPath.row]
+        cell.lbRestaurantName.text = order.restaurantName
+        cell.lbCustomerName.text = order.customerName
+        cell.lbCustomerAddress.text = order.customerAddress
+        
+        cell.imgCustomerAvatar.image = try! UIImage(data: Data(contentsOf: URL(string: order.customerAvatar!)!))
+        cell.imgCustomerAvatar.layer.cornerRadius = 50/2
+        cell.imgCustomerAvatar.clipsToBounds = true
         
         return cell
     }
